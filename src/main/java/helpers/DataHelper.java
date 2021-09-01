6402b17e68cf369d1151178ac694e0b1a43121f5
unknown <trangnguyen2601@gmail.com>
2021-09-01 13:55:04 +0700

    removed redundant code

--- src/main/java/helpers/DataHelper.java
+++ src/main/java/helpers/DataHelper.java
@@ -4,19 +4,4 @@ import com.github.javafaker.Faker;
 import java.util.Random;
 
 public class DataHelper {
-
-    private static final Faker faker = new Faker();
-    private static Random randomGenerator = new Random();
-
-    public static String getRandomEmail() {
-        return faker.internet().emailAddress();
-    }
-
-    public static String getRandomPassword() {
-        return faker.letterify("????????");
-    }
-
-    public static String getRandomNumber() {
-        return faker.numerify("########");
-    }
 }
